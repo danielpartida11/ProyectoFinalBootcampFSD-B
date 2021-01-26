83306732ebe9c8aa29a87c9e766f421448df64c6
unknown <danielpartida32@hotmail.com>
2021-01-26 20:53:34 +0100

    completado el modelo cars con los campos de su tabla correspondiente

--- app/Models/Cars.php
+++ app/Models/Cars.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Cars extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'model',
+        'fuel',
+        'km',
+        'tank',
+        'prize',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
 }
